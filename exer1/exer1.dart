b8a0604566dd492d624878bf8cc59c247f5b2c92
João Assef <joaoassef@gmail.com>
2025-10-02 21:17:32 -0300

    Exercício aula 1 DART

---
+++ exer1/exer1.dart
@@ -0,0 +1,11 @@
+import 'dart:math';
+
+//Crie um programa em Dart que calcula e imprime a área de um círculo cujo raio é 5.75 cm.
+
+void main() {
+  double raio = 5.75;
+  double circ = 2 * raio * pi;
+
+  print('Crie um programa em Dart que calcula e imprime a área de um círculo cujo raio é 5.75 cm');
+  print('Resposta: $circ');
+}
