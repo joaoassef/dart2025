bec80d51ed5685fc0e1d4c4a95a1f9c2744e18c7
João Assef <joaoassef@gmail.com>
2025-10-02 23:03:33 -0300

    Create ex2.dart

---
+++ exer1/ex2.dart
@@ -0,0 +1,15 @@
+void main() {
+  DateTime agora = DateTime.now();
+
+  int informado = 12;
+  int atual = agora.month;
+
+  if (informado > atual) {
+    print('O mês $atual é menor que o mês $informado');
+  } else if (informado == atual) {
+    print('O mês $atual é igual ao mês $informado');
+  } else {
+    print('O mês $atual é maior que o mês $informado');
+  }
+  
+}
